9876466638ddfefddb38ba5b45cdd81c6546b265
Nikolas Lacerda <nikolas.lacerda@hotmail.com>
2020-10-13 17:08:04 -0300

    add email notification

--- server/src/main/java/com/usermanagement/server/constant/EmailConstant.java
+++ server/src/main/java/com/usermanagement/server/constant/EmailConstant.java
@@ -2,8 +2,8 @@ package com.usermanagement.server.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "email@gmail.com";
-    public static final String PASSWORD = "your password";
+    public static final String USERNAME = "your_email@gmail.com";
+    public static final String PASSWORD = "your_password";
     public static final String FROM_EMAIL = "support@nikolas.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "User Management - New Password";
